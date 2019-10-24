4c389cd08627ca1c704ac857f17c819c549c857b
antonio_pertuz <00267016@uca.edu.sv>
2019-10-24 14:08:02 -0600

    Badge component complete

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import confLogo from '../images/badge-header.svg'
+import confLogo from '../images/badge-header.svg';
+import "./styles/Badge.css";
+
 class Badge extends React.Component {
     render() {
         return(
-            <div>
-                <div>
+            <div className={"Badge"}>
+                <div className={"Badge__header"}>
                     <img src={confLogo} alt="Logo de la conferencia"/>
                 </div>
-                <div>
-                    <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar"/>
-                    <h1>Antonio<br/>Pertuz</h1>
+                <div className={"Badge__section-name"}>
+                    <img className={"Badge__avatar"} src={this.props.avatarImage} alt="Avatar"/>
+                    <h1>{this.props.firstName}<br/>{this.props.lastName}</h1>
                 </div>
-                <div>
-                    <p>Engineer</p>
-                    <p>@antonio</p>
+                <div className={"Badge__section-info"}>
+                    <h3>{this.props.jobTitle}</h3>
+                    <div>@{this.props.twitterAccount}</div>
                 </div>
-                <div>
+                <div className={"Badge__footer"}>
                     #catechconf
                 </div>
             </div>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "bootstrap/dist/css/bootstrap.css";
+
+import "./global.css"
 import Badge from "./components/Badge";
 
 const container = document.getElementById('app');
 
-ReactDOM.render(<Badge/>, container);
+ReactDOM.render(
+    <Badge
+        firstName={"Antonio"}
+        lastName={"Pertuz"}
+        jobTitle={"Engineer"}
+        avatarImage={"https://www.gravatar.com/avatar?d=identicon"}
+        twitterAccount={"antonioace7"}
+    />,
+    container);
