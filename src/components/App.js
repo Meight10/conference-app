0ceefaff8973a5a788137c0f8926e69ed07e3a2e
antonio_pertuz <00267016@uca.edu.sv>
2019-11-10 08:28:26 -0600

    Home route added

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 import BadgeNew from "../pages/BadgeNew";
 import Badges from '../pages/Badges';
 import Layout from "./Layout";
 import NotFound from "../pages/NotFound";
+import Home from "../pages/Home";
 
 const App = () => {
         return (
             <BrowserRouter>
                 <Layout>
                     <Switch>
+                        <Route exact path={"/"} component={Home} />
                         <Route exact path={"/badges"} component={Badges} />
                         <Route exact path={"/badges/new"} component={BadgeNew} />
                         <Route component={NotFound} />
